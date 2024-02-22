1f341d11783f0f84c1ad80afcfb77bbabd29e119
sri sai <gandusrisai@gmail.com>
2024-02-22 23:42:34 +0530

    Thoda Warm up kardiya

---
+++ ArrayBasics.cs
@@ -0,0 +1,72 @@
+public class ArrayBasics{
+
+    // Quick Sort, Merge Sort,... etc these works with O(nlogn)
+    public int LargestElementInArray(int[] array)
+    {
+        Array.Sort(array);
+        return array[array.Length-1];
+    }
+
+    // O(n) optimal solution
+    public int LargestElementInArrayV1(int[] array){
+        int max = int.MinValue;
+        int i = 0;
+        while(i<array.Length){
+            if(array[i]>max)
+                max = array[i];
+            i++;
+        }
+        return max;
+    }
+
+    // O(nlogn)+O(N) if that sorting is working like quick sort, merge sort etc...
+    public int SecondLargestElementInArray(int[] array)
+    {
+        Array.Sort(array);
+        int i = array.Length - 1;
+        while(i>0){
+            if(array[i]!=array[i-1]){
+                return array[i-1];
+            }
+            i--;        
+        }
+        return array[i];
+    }
+
+    // O(n)+O(n) = O(n)
+    public int SecondLargestElementInArrayV1(int[] array)
+    {
+        int largest = LargestElementInArrayV1(array);
+        int secondLargest = int.MinValue;
+        int i = 0 ;
+        while(i<array.Length){
+            if(array[i]>secondLargest && array[i] != largest)
+                secondLargest = array[i];
+            i++;
+        }
+        return secondLargest;
+    }
+
+// O(n)
+    public int SecondLargestElementInArrayV2(int[] array)
+    {
+        int i = 1;
+        int largest = array[0];
+        int secondLargest = int.MinValue;
+        while (i < array.Length)
+        {
+            if(array[i]>largest){
+                secondLargest = largest;
+                largest = array[i];
+            }
+            else if(array[i]<largest && array[i]>secondLargest){
+                secondLargest = array[i];
+            }
+            i++;
+        }
+        return secondLargest;
+    }
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -2,10 +2,17 @@
 {
     private static void Main(string[] args)
     {
-        int[] array = new int[]{14,9,15,12,6,8,13};
-        Sorting sorting = new();
-        var sortedArray = sorting.InsertionSort(array);
-        Console.WriteLine(string.Join("",sortedArray));
+        int[] array = new int[]{14,9,12,6,8,14,14,13};
+        ArrayBasics arrayBasics = new ArrayBasics();
+        var result = arrayBasics.SecondLargestElementInArrayV2(array);
+        Console.WriteLine(result);
+
+
+
+
+        // Sorting sorting = new();
+        // var sortedArray = sorting.InsertionSort(array);
+        // Console.WriteLine(string.Join("",sortedArray));
     }
     
 }
